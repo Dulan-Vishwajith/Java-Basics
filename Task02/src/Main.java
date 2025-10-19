27b5b4d2cf67c9b75eefbfeb35989097ad2f0db5
Dulan Vishwajith <dulanvishwajithgunarathna@gmail.com>
2025-10-19 11:59:22 +0530

    Initial commit

---
+++ Task02/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args){
+        String name="Jhone";
+        int age=22;
+        double marks=79;
+        char grade='A';
+        boolean isPass=true;
+
+
+        System.out.println(name);
+        System.out.println(marks);
+        System.out.println(grade);
+        if(isPass){
+            System.out.println("Pass");
+        }
+    }
+}
