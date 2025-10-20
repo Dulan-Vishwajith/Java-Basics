f8a1bbe157d9d388a60bd7e94cfaaac62979b1cd
Dulan Vishwajith <dulanvishwajithgunarathna@gmail.com>
2025-10-20 22:09:48 +0530

    Initial commit

---
+++ Shopping Cart Program/src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] ars){
+        Scanner input=new Scanner(System.in);
+
+        String  item;
+        double price;
+        int quantity;
+        String currency ="LKR";
+        double total;
+
+        System.out.print("What item would you like to buy? : ");
+        item=input.nextLine();
+
+        System.out.print("What is the price for each? : ");
+        price=input.nextDouble();
+
+        System.out.print("How many would you like? : ");
+        quantity= input.nextInt();
+        input.nextLine();
+
+        total=price * quantity;
+
+        System.out.println("\nYou have bought "+quantity+" "+item+"/s");
+        System.out.println("Your total is "+currency+" "+total);
+
+
+        input.close();
+    }
+}
